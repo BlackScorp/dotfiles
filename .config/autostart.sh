be863b525e5e4df69aca60c91fe1210e49103abc
BlackScorp <cccpmik@gmail.com>
2026-03-15 14:35:32 +0100

    add more autostart services

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,4 +1,9 @@
+exec pipewire &
+exec pipewire-pulse &
+exec wireplumber &
 exec swww-daemon &
 cd $HOME/.config/swww/ && exec ./update-background &
 exec eww daemon &
 exec eww open bar --no-daemonize &
+exec keepassxc --minimized &
+#exec /usr/libexec/polkit-gnome-authentication-agent-1 &
