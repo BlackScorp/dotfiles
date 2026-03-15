296272c08436a4d42b8de653d05fe50fd9f942e0
BlackScorp <cccpmik@gmail.com>
2026-03-15 21:45:42 +0100

    updates

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -5,5 +5,4 @@ exec swww-daemon &
 cd $HOME/.config/swww/ && exec ./update-background &
 exec eww daemon &
 exec eww open bar --no-daemonize &
-exec keepassxc --minimized &
-#exec /usr/libexec/polkit-gnome-authentication-agent-1 &
+#exec keepassxc --minimized &
